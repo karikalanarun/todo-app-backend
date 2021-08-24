144e00cc90ed418405603601a2d73aa870a0ed8e
Arun Karikalan <arun.k@skitter.in>
2021-08-24 15:16:14 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,55 @@
+
+const express = require("express");
+const app = express()
+const fs = require("fs")
+const { v4: uuid } = require("uuid")
+const util = require("util")
+
+const readFile = util.promisify(fs.readFile)
+const writeFile = util.promisify(fs.writeFile)
+
+function getTodos() {
+    return readFile("./data.json", "utf-8").then(JSON.parse)
+}
+
+function writeTodos(todos) {
+    return writeFile("./data.json", todos, "utf-8")
+}
+
+app.use(express.json())
+
+app.post("/todo", async (req, res) => {
+    const { todos } = await getTodos()
+    let id = uuid()
+    let todo = {
+        id,
+        ...req.body
+    };
+    await writeTodos(JSON.stringify({
+        todos: [...todos, todo]
+    }))
+    res.send(todo)
+})
+
+app.get("/todos", async (req, res) => {
+    res.send(await getTodos())
+})
+
+app.delete("/todo/:id", async (req, res) => {
+    const { todos } = await getTodos()
+    let index = todos.findIndex(({ id }) => req.params.id)
+    await writeTodos(JSON.stringify({ todos: [...todos.slice(0, index), ...todos.slice(index + 1)] }))
+    res.sendStatus(200)
+})
+
+app.put("/todo/:id", async (req, res) => {
+    const { todos } = await getTodos()
+    let index = todos.findIndex(({ id }) => req.params.id)
+    await writeTodos(JSON.stringify({ todos: [...todos.slice(0, index), { ...todos[index], ...req.body }, ...todos.slice(index + 1)] }))
+    res.sendStatus(200)
+})
+
+
+app.listen(3001, () => {
+    console.log("server is up and running on http://localhost:3001")
+})
